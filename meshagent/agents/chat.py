819f6af6bd6f264cd11c5e9677d89302cd25c399
Jesse Ezell <jesse.ezell@timu.com>
2025-02-25 06:14:39 -0800

    merge repos

---
+++ meshagent/agents/chat.py
@@ -0,0 +1,293 @@
+import urllib.parse
+from .agent import SingleRoomAgent, AgentChatContext
+from meshagent.api.chan import Chan
+from meshagent.api import RoomMessage, RoomException, RoomClient, RemoteParticipant
+from meshagent.tools import Toolkit
+from .adapter import LLMAdapter, ToolResponseAdapter
+import asyncio
+from typing import Optional
+import json
+import logging
+from meshagent.tools import MultiToolkit
+
+from meshagent.openai import OpenAILLMAdapter
+from openai import AsyncOpenAI
+import urllib
+
+logging.basicConfig()
+logger = logging.getLogger("chat")
+logger.setLevel(logging.INFO)
+
+
+# todo: thread should stop when participant stops?
+
+class ChatBot(SingleRoomAgent):
+    def __init__(self, *, name, title = None, description = None, requires = None,  llm_adapter: LLMAdapter, tool_adapter: ToolResponseAdapter, toolkits: Optional[list[Toolkit]] = None, rules : Optional[list[str]] = None, auto_greet_prompt : Optional[str] = None, auto_greet_message : Optional[str] = None,  empty_state_title : Optional[str] = None, labels: Optional[str] = None):
+        super().__init__(
+            name=name,
+            title=title,
+            description=description,
+            requires=requires,
+            labels=labels
+        )
+
+        if toolkits == None:
+            toolkits = []
+
+        self._llm_adapter = llm_adapter
+        self._tool_adapter = tool_adapter
+
+        self._message_channels = dict[str, Chan[RoomMessage]]()
+
+        self._room : RoomClient | None = None
+        self._toolkits = toolkits
+
+        if rules == None:
+            rules = []
+
+        self._rules = rules
+        self._is_typing = dict[str,asyncio.Task]()
+        self._auto_greet_prompt = auto_greet_prompt
+        self._auto_greet_message = auto_greet_message
+        
+        if empty_state_title == None:
+            empty_state_title = "How can I help you?"
+        self._empty_state_title = empty_state_title
+
+
+    async def greet(self, *, chat_context: AgentChatContext, messages: Chan[RoomMessage], participant: RemoteParticipant):
+
+        if self._auto_greet_prompt != None:
+            messages.send_nowait(RoomMessage(from_participant_id=participant.id, type="chat",  message={"text": self._auto_greet_prompt }))
+        
+        if self._auto_greet_message != None:
+            chat_context.append_user_message(self._auto_greet_message)
+            
+            await self.room.messaging.send_message(to=RemoteParticipant(id=participant.id), type="chat", message={ "text" : self._auto_greet_message  })
+
+
+    def finalize_toolkits(self, toolkits) -> list[Toolkit]:
+
+        toaster = None
+        
+        for toolkit in toolkits:
+
+            if toolkit.name == "meshagent.ui":
+
+                for tool in toolkit.tools:
+
+                    if tool.name == "show_toast":
+
+                        toaster = tool
+
+        if toaster != None:
+
+            def multi_tool(toolkit: Toolkit):
+                if toaster in toolkit.tools:
+                    return toolkit
+                
+                return MultiToolkit(required=[ toaster ], base_toolkit=toolkit )
+
+            toolkits = list(map(multi_tool, toolkits))
+        
+        return toolkits
+
+    async def _spawn_thread(self, participant_id: str, messages: Chan[RoomMessage]):
+
+        
+        chat_context = await self.init_chat_context()
+        
+
+        chat_context.append_rules(
+            rules=[
+                *self._rules,
+                "think step by step",
+           ]
+        )
+
+        opened = False
+        chat_with_participant = None
+
+        for participant in self._room.messaging.get_participants():
+            if participant.id == participant_id:
+                chat_with_participant = participant
+                break
+
+        if chat_with_participant == None:
+            raise RoomException(f"caller did not have messaging turned on")
+        
+        messaging = self._room.messaging
+        
+        step_schema = {
+            "type" : "object",
+            "required" : ["text","finished"],
+            "additionalProperties" : False, 
+            "description" : "execute a step",
+            "properties" : {
+                "text" : {
+                    "description" : "a reply to the user or status to display during an intermediate step",
+                    "type" : "string"
+                },
+                "finished" : {
+                    "description" : "whether the agent has finished answering the user's last message, also should be set to true if we get stuck in a loop, or if the user did not make a request",
+                    "type" : "boolean"
+                }
+            }
+        }
+
+        installed = False
+
+        while True:
+            
+            while True:
+
+                received = await messages.recv()
+
+                if installed == False:
+                    installed = True
+                    await self.install_requirements(participant_id=participant_id)
+
+                if received.type == "opened":
+                    
+                    
+                    if opened == False:
+                        
+                        opened = True
+                        
+                        await self.greet(chat_context=chat_context, participant=chat_with_participant, messages=messages)
+
+                if received.type == "chat":
+
+
+                    await self._room.messaging.send_message(to=chat_with_participant, type="thinking", message={"thinking":True})
+                
+                    if chat_with_participant.id == received.from_participant_id:
+                        self.room.developer.log_nowait(type="llm.message", data={ "context" : chat_context.id, "participant_id" : self.room.local_participant.id, "participant_name" : self.room.local_participant.get_attribute("name"), "message" : { "content" : {  "role" : "user", "text" : received.message["text"] } } })
+                
+                        chat_context.append_user_message(message=received.message["text"])
+                            
+
+                    # if user is typing, wait for typing to stop
+                    while True:
+                        
+                        if chat_with_participant.id not in self._is_typing:
+                            break
+                    
+                        await asyncio.sleep(.5)
+
+                    if messages.empty() == True:
+                        break
+        
+
+            try:
+                while True:
+
+                    toolkits = [
+                        *self._toolkits,
+                        *await self.get_required_tools(participant_id=chat_with_participant.id)
+                    ]
+
+                    toolkits = self.finalize_toolkits(toolkits)
+
+                    response = await self._llm_adapter.next(
+                        context=chat_context,
+                        room=self._room,
+                        toolkits=toolkits,
+                        tool_adapter=self._tool_adapter,
+                        output_schema=step_schema,
+                    )
+
+                    text = response["text"]
+                    
+                    
+                    if response["finished"] or len(toolkits) == 0:
+                        await self._room.messaging.send_message(
+                            to=chat_with_participant,
+                            type="chat",
+                            message={
+                                "text": text
+                            }
+                        )
+                        break
+                    else:
+                        await self._room.messaging.send_message(
+                            to=chat_with_participant,
+                            type="status",
+                            message={
+                                "text": text
+                            }
+                        )
+                        chat_context.append_user_message(message="proceed to the next step if you are ready")
+                
+            finally:
+
+                await self._room.messaging.send_message(to=chat_with_participant, type="thinking", message={"thinking":False})
+
+   
+
+    def _get_message_channel(self, participant_id: str) -> Chan[RoomMessage]:
+        if participant_id not in self._message_channels:
+            chan = Chan[RoomMessage]()
+            self._message_channels[participant_id] = chan
+
+            def thread_done(task: asyncio.Task):
+
+                self._message_channels.pop(participant_id)
+                try:
+                    task.result()
+                    logger.info("ending chat thread")
+                except Exception as e:
+                    logger.error("chat thread error", exc_info=e)
+
+            task = asyncio.create_task(self._spawn_thread(participant_id=participant_id, messages=chan))
+            task.add_done_callback(thread_done)
+        
+        chan = self._message_channels[participant_id]
+        
+        return chan
+    
+    async def start(self, *, room):
+
+        await super().start(room=room)
+
+        await self.room.local_participant.set_attribute("empty_state_title", self._empty_state_title)
+
+        def on_message(message: RoomMessage):
+            messages = self._get_message_channel(participant_id=message.from_participant_id)
+            if message.type == "chat" or message.type == "opened":
+                messages.send_nowait(message)
+    
+            elif message.type == "typing":
+
+                
+                def callback(task: asyncio.Task):
+                    try:
+                        task.result()
+                    except:
+                        pass
+                
+                async def remove_timeout(id: str):
+                    await asyncio.sleep(1)
+                    self._is_typing.pop(id)
+
+                if message.from_participant_id in self._is_typing:
+                    self._is_typing[message.from_participant_id].cancel()
+
+                timeout = asyncio.create_task(remove_timeout(id=message.from_participant_id))
+                timeout.add_done_callback(callback)
+
+                self._is_typing[message.from_participant_id] = timeout
+
+        room.messaging.on("message", on_message)
+        
+        if self._auto_greet_prompt != None or self._auto_greet_message != None:
+            def on_participant_added(participant:RemoteParticipant):
+                
+                # will spawn the initial thread
+                self._get_message_channel(participant_id=participant.id)
+               
+
+            room.messaging.on("participant_added", on_participant_added)
+
+        await room.messaging.enable()
+
---
+++ meshagent/agents/indexer.py
@@ -0,0 +1,340 @@
+from meshagent.agents import TaskRunner, RequiredToolkit
+from meshagent.tools import Toolkit, Tool, ToolContext
+from meshagent.api.room_server_client import TextDataType, VectorDataType, FloatDataType, IntDataType
+from openai import AsyncOpenAI
+from typing import Optional
+import hashlib
+import chonkie
+import asyncio
+import logging
+
+# TODO: install chonkie, chonkie[semantic], openai
+
+from concurrent.futures import ThreadPoolExecutor, as_completed
+
+logger = logging.getLogger("indexer")
+logger.setLevel(logging.INFO)
+
+class Chunk:
+    def __init__(self, *, text: str, start: int, end: int):
+        self.text = text
+        self.start = start
+        self.end = end
+
+class Chunker:
+
+    async def chunk(self, *, text: str, max_length: Optional[int] = None) -> list[Chunk]:
+        pass
+
+class ChonkieChunker(Chunker):
+    def __init__(self, chunker: Optional[chonkie.BaseChunker] = None):
+        super().__init__()
+
+        if chunker == None:
+            chunker = chonkie.SemanticChunker()
+
+        self._chunker = chunker
+
+    async def chunk(self, *, text: str, max_length: Optional[int] = None) -> list[Chunk]:
+        chunks = await asyncio.to_thread(self._chunker.chunk, text=text)
+        mapped = []
+        for chunk in chunks:
+            mapped.append(Chunk(text=chunk.text, start=chunk.start_index, end=chunk.end_index))
+        return mapped
+    
+
+class Embedder:
+    def __init__(self, *, size: int, max_length: int):
+        self.size = size
+        self.max_length = max_length
+    
+    async def embed(self, *, text: str) -> list[float]:
+        pass
+
+class OpenAIEmbedder(Embedder):
+    def __init__(self, *, size: int, max_length: int, model: str,  openai: Optional[AsyncOpenAI] = None, ):
+        if openai == None:
+            openai = AsyncOpenAI()
+    
+        self._openai = openai
+        self._model = model
+
+        super().__init__(size=size, max_length=max_length)
+
+    
+    async def embed(self, *, text):
+        return (await self._openai.embeddings.create(input=text, model=self._model, encoding_format="float")).data[0].embedding
+    
+
+open_ai_embedding_3_small = OpenAIEmbedder(model="text-embedding-3-small", max_length=8191, size=1536)
+open_ai_embedding_3_large = OpenAIEmbedder(model="text-embedding-3-large", max_length=8191, size=3072)
+open_ai_embedding_ada_2 = OpenAIEmbedder(model="text-embedding-ada-002", max_length=8191, size=1536)
+
+
+class RagTool(Tool):
+    def __init__(self, *, name = "rag_search", table: str, title = "RAG search", description = "perform a RAG search", rules = None, thumbnail_url = None, embedder: Optional[Embedder] = None):
+        
+        self.table = table
+
+        super().__init__(
+            name=name,
+            input_schema={
+                "type":"object",
+                "additionalProperties" : False,
+                "required" : [
+                    "query"
+                ],
+                "properties" : {
+                    "query" : {
+                        "type" : "string"
+                    }
+                }
+            },
+            title=title,
+            description=description,
+            rules=rules, thumbnail_url=thumbnail_url)
+        
+        self._embedder = embedder
+
+    async def execute(self, context: ToolContext, query: str):
+        
+        if self._embedder == None:
+            results = await context.room.database.search(
+                table=self.table,
+                text=query,
+                limit=10
+            )
+        else:
+            embedding = await self._embedder.embed(text=query)
+            results = await context.room.database.search(
+                table=self.table,
+                text=query,
+                vector=embedding,
+                limit=10
+            )
+
+        results = list(map(lambda r: f"from {r["url"]}: {r["text"]}", results))
+
+        return {
+            "results" : results
+        }
+        
+
+class RagToolkit(Toolkit):
+    def __init__(self, table: str, embedder:Optional[Embedder] = None):
+
+        if embedder == None:
+            embedder = open_ai_embedding_3_large
+
+        super().__init__(
+            name="meshagent.rag",
+            title="RAG",
+            description="Searches against an index",
+            tools=[
+                RagTool(table=table, embedder=embedder)
+            ]
+        )
+
+
+class SiteIndexer(TaskRunner):
+
+    def __init__(self,
+         *, 
+        name,
+        chunker: Optional[Chunker] = None,
+        embedder:Optional[Embedder] = None,
+        title=None,
+        description=None,
+        requires=None,
+        supports_tools = None,
+        labels: Optional[list[str]] = None
+     
+    ):
+        
+        if chunker == None:
+            chunker = ChonkieChunker()
+
+        if embedder == None:
+            embedder = open_ai_embedding_3_large
+
+        self.chunker = chunker
+        self.embedder = embedder
+
+        super().__init__(
+            name=name,
+            title=title,
+            description=description,
+            requires=[
+                RequiredToolkit(
+                    name="meshagent.firecrawl",
+                    tools=[
+                        "firecrawl_queue"
+                    ]
+                ),
+            ],
+            supports_tools=supports_tools,
+            input_schema={
+                "type" : "object",
+                "required" : [
+                    "queue", "table", "url"
+                ],
+                "additionalProperties" : False,
+                "properties" : {
+                    "queue" : {
+                        "type" : "string",
+                        "description" : "default: firecrawl"
+                    },
+                    "table" : {
+                        "type" : "string",
+                        "description" : "default: index"
+                    },
+                    "url" : {
+                        "type" : "string",
+                        "description" : "default: index"
+                    }
+                }
+            },
+            output_schema={
+                "type" : "object",
+                "required" : [],
+                "additionalProperties" : False,
+                "properties" : {},
+            },
+            labels=labels
+        )
+
+
+    async def ask(self, *, context, arguments):
+        
+        queue = arguments["queue"]
+        table = arguments["table"]
+        url = arguments["url"]
+
+        tables = await context.room.database.list_tables()
+
+        exists = False
+        try:
+            exists = tables.index(table)
+        except ValueError:
+            pass
+
+
+        async def lookup_or_embed(*, sha: str, text: str) -> list[float]:
+
+            # if we already indexed this chunk, lets use the existing embedding instead of generating a new one
+            if exists:
+
+                results = await context.room.database.search(where={
+                    "table" : table,
+                    "sha" : sha,
+                    "limit" : 1
+                })
+
+                if len(results) != 0:
+                    logger.info(f"chunk found from {url} {sha}: {text}, reusing embedding")
+                    return results[0]["embedding"]
+                
+            logger.info(f"chunk not found from {url} {sha}: {text}, generating embedding")
+                    
+            return await self.embedder.embed(text=text)
+            
+        
+        async def crawl():
+            logger.info(f"starting to crawl: {url}")
+            await context.room.agents.invoke_tool(
+                toolkit="meshagent.firecrawl",
+                tool="firecrawl_queue",
+                arguments={
+                    "url" : url,
+                    "queue": queue,
+                    "limit" : 100
+                })
+            
+            logger.info(f"done with crawl: {url}")
+            await context.room.queues.send(name=queue, message={ "done" : True })
+            
+        def crawl_done(task: asyncio.Task):
+            try:
+                task.result()
+            except Exception as e:
+                logger.error("crawl failed", exc_info=e)
+
+
+        crawl_task = asyncio.create_task(crawl())
+        crawl_task.add_done_callback(crawl_done)
+        
+        rows = []
+
+        id = 0
+        
+        while True:
+            message = await context.room.queues.receive(name=queue, create=True, wait=True)
+            try:
+                if message == None:
+                    break
+
+                if message.get("done", False) == True:
+                    break
+            
+                url : str  = message["data"]["metadata"]["url"]
+                text : str = message["data"]["markdown"]
+                title : str  = message["data"]["metadata"]["title"]
+                title_sha : str  = hashlib.sha256(text.encode("utf-8")).hexdigest()
+
+                logger.info(f"processing crawled page: {url}")
+                
+                # let's make the title it's own chunk
+                rows.append(
+                        {
+                            "id" : id,
+                            "url" : url,
+                            "text" : title,
+                            "sha" : title_sha,
+                            "embedding" : await lookup_or_embed(sha=title_sha, text=title)
+                        }
+                    )
+                    
+                id = id + 1
+                
+                # the content will be transformed into additional chunks
+                for chunk in await self.chunker.chunk(text=text, max_length = self.embedder.max_length):
+                    logger.info(f"processing chunk from {url}: {chunk.text}")
+                    chunk_sha = hashlib.sha256(chunk.text.encode("utf-8")).hexdigest()
+                    rows.append(
+                        {
+                            "id" : id,
+                            "url" : url,
+                            "text" : chunk.text,
+                            "embedding" : await lookup_or_embed(sha=chunk_sha, text=chunk.text)
+                        }
+                    )
+                    
+                    id = id + 1
+
+            except Exception as e:
+                logger.error(f"failed to process: {url}", exc_info=e)
+
+        logger.info(f"saving crawl: {url}")
+            
+        await context.room.database.create_table_with_schema(
+            name=table,
+            schema={
+                "id" : IntDataType(),
+                "url" : TextDataType(),
+                "text" : TextDataType(),
+                "embedding" : VectorDataType(
+                    size=self.embedder.size,
+                    element_type=FloatDataType()
+                ),
+                "sha" : TextDataType(),
+            },
+            mode="overwrite",
+            data=rows
+        )
+
+        if len(rows) > 255:
+            await context.room.database.create_vector_index(table=table, column="embedding")
+
+        await context.room.database.create_full_text_search_index(table=table, column="text")
+
+        return {}
