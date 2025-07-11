fa236b0e224a08dbb9554c9e750bd003d22eb273
Jesse Ezell <jesse.ezell@timu.com>
2025-07-11 11:18:26 -0700

    add meshagent[otel], fix document_schema import

--- meshagent/agents/schemas/__init__.py
+++ meshagent/agents/schemas/__init__.py
@@ -1,7 +1,7 @@
-from document import document_schema
-from gallery import gallery_schema
-from presentation import presentation_schema
-from super_editor_document import super_editor_document_schema
+from .document import document_schema
+from .gallery import gallery_schema
+from .presentation import presentation_schema
+from .super_editor_document import super_editor_document_schema
 
 __all__ = [
     document_schema,
