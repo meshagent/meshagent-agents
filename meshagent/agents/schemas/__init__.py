7d0c51cc12b19b5ce66f0d625c3605f6572c8721
jesse ezell <jezell@gmail.com>
2026-06-30 09:18:56 -0700

    Update runtime, SDK, and usage reporting

--- meshagent/agents/schemas/__init__.py
+++ meshagent/agents/schemas/__init__.py
@@ -4,8 +4,8 @@ from .presentation import presentation_schema
 from .super_editor_document import super_editor_document_schema
 
 __all__ = [
-    document_schema,
-    gallery_schema,
-    presentation_schema,
-    super_editor_document_schema,
+    "document_schema",
+    "gallery_schema",
+    "presentation_schema",
+    "super_editor_document_schema",
 ]
